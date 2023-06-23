09c947e370d5e83fc23fa3c3de62dd5ac8b23d85
YousukeTakenaka <takenaka110928@gmail.com>
2023-06-23 15:54:04 +0900

    Create try04.php

---
+++ htdocs/try04/try04.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>TRY04</title>
+    </head>
+    <body>
+        <?php
+            $num;
+            $num = 1 + 2;
+           
+            $num
+    </body>
+</html>
