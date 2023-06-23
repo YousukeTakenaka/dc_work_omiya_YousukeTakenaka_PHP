96553f6c9d145bb93f86092563772b31887a8cb9
YousukeTakenaka <takenaka110928@gmail.com>
2023-06-23 15:54:15 +0900

    Create work02.php

---
+++ htdocs/work02/work02.php
@@ -0,0 +1,10 @@
+<?php
+    const height = 178;
+    const weight = 58;
+
+    print '変更前：身長'.height.'cm 体重'.weight.'kg';
+
+    height = 185;
+    weight = 67;
+
+    print '変更後：身長'.height.'cm 体重'.weight.'kg';
