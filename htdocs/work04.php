5bc5df0cb757cf7b20a0e48abb7828ebc0e09723
YousukeTakenaka <takenaka110928@gmail.com>
2023-06-29 19:04:01 +0900

    Create work04.php

---
+++ htdocs/work04.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>WORK04</title>
+    </head>
+    <body>
+        <?php
+            $result = (109439032 + 39443083) / 4934;
+            print '<p>計算の答えは'.$result.'です。</p>';
+        ?>
+    </body>
+</html>
