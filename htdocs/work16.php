fbd8bce24fa70092dfa2e899894737d3f83231e4
YousukeTakenaka <takenaka110928@gmail.com>
2023-07-18 11:16:26 +0900

    Create work16.php

---
+++ htdocs/work16.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>WORK16</title>
+    </head>
+    <body>
+        <form method="get" action="work16_02.php">
+            <p>名前：<br>
+            <input type="text" name="display_text"></p>
+            <input type="checkbox" name="check[]" value="選択肢1">選択肢01
+            <input type="checkbox" name="check[]" value="選択肢2">選択肢02
+            <input type="checkbox" name="check[]" value="選択肢3">選択肢03
+            <p><input type="submit" value="送信"></p>
+        </form>
+    </body>
+</html>
