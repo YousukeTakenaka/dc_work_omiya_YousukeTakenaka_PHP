e1df4394f1b93fd3bb64fb302f6e679fc010d344
YousukeTakenaka <takenaka110928@gmail.com>
2023-07-19 21:19:10 +0900

    Create work19.php

---
+++ htdocs/work19.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>WORK19</title>
+    </head>
+    <body>
+        <form method="post">
+            <p>タイトル<br>
+            <input type="text" name="title"></p>
+            <p>書き込み内容<br>
+            <input type="text" name="main"></p>
+            <br>
+            <input type="submit" value="送信">
+        </form>
+    </body>
+</html>
