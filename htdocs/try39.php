e05a0f48a9c56176bdd87d5425355bf686faafd0
YousukeTakenaka <takenaka110928@gmail.com>
2023-07-28 13:56:24 +0900

    Create try39.php

---
+++ htdocs/try39.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>TRY39</title>
+    </head>
+    <body>
+        <?php
+            $db = new mysqli('mysql34.conoha.ne.jp', 'bcdhm_omiya_pf0014', 'x4Xh7n(%', 'bcdhm_omiya_pf0014');
+            if ($db->connect_error) {
+                echo $db->connect_error;
+                exit();
+            } else {
+                $db->set_charset("utf8");
+            }
+
+            $sql = "SELECT product_name, price FROM product WHERE price <= 100";
+            if ($result = $db->query($sql)) {
+                foreach ($result as $row) {
+                    echo $row["product_name"] . $row["price"] . "<br>";
+                }
+                $result->close();
+            }
+            $db->close();
+        ?>
+    </body>
+</html>
